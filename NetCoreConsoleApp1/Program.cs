8efb99303ca4463b1a1e3744b06c35db7134245f
Susie Zhou (CSI Interfusion Inc) <v-yuwzh@microsoft.com>
2021-01-11 21:40:38 -0800

    Add project files.

---
+++ NetCoreConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NetCoreConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello from console app ");
+            Console.ReadKey();
+        }
+    }
+}
